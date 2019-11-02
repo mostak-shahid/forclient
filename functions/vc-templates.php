11f75c352b79f9b06a390d93e80c68f87206c4c4
Md. Mostak Shahid <mostak.shahid@gmail.com>
2019-11-02 12:02:27 +0600

    Update vc-templates.php

--- functions/vc-templates.php
+++ functions/vc-templates.php
@@ -1,107 +1,53 @@
 <?php
-// Before VC Init
-add_action( 'vc_before_init', 'vc_before_init_actions' );
- 
-function vc_before_init_actions() {
-     
-    // Put Code Here ..//
- 
-    // Including the separate file created here
-    require_once( get_template_directory().'/vc-elements/my-first-own-custom-element.php' ); 
-     
+function bartag_func( $atts = array(), $content = '' ) {
+	$atts = shortcode_atts( array(
+		'foo' => 'something',
+		'color' => '#FFF'
+	), $atts, 'bartag' );	
+	return '<div style="color:'.$atts['color'].'" data-foo="'.$atts['foo'].'">'.$content.'</div>';
 }
-/*
-Element Description: VC Info Box
-*/
- 
-// Element Class 
-class vcInformationBox extends WPBakeryShortCode {
-     
-    // Element Init
-    function __construct() {
-        add_action( 'init', array( $this, 'vc_informationbox_mapping' ) );
-        add_shortcode( 'vc_informationbox', array( $this, 'vc_informationbox_html' ) );
-    }
-	// Element Mapping
-	public function vc_informationbox_mapping() {
-	         
-	    // Stop all if VC is not enabled
-	    if ( !defined( 'WPB_VC_VERSION' ) ) {
-	            return;
-	    }
-	         
-	    // Map the block with vc_map()
-	    vc_map( 
-	  
-	        array(
-	            'name' => __('VC Informationbox', 'text-domain'),
-	            'base' => 'vc_informationbox',
-	            'description' => __('A simple VC box', 'text-domain'), 
-	            'category' => __('My Own Custom Elements', 'text-domain'),   
-	            'icon' => get_template_directory_uri().'/assets/img/vc-icon.png',            
-	            'params' => array(   
-	                      
-	                array(
-	                    'type' => 'textfield',
-	                    'holder' => 'h3',
-	                    'class' => 'title-class',
-	                    'heading' => __( 'Title', 'text-domain' ),
-	                    'param_name' => 'title',
-	                    'value' => __( 'Default value', 'text-domain' ),
-	                    'description' => __( 'Box Title', 'text-domain' ),
-	                    'admin_label' => false,
-	                    'weight' => 0,
-	                    'group' => 'Custom Group',
-	                ),  
-	                  
-	                array(
-	                    'type' => 'textarea',
-	                    'holder' => 'div',
-	                    'class' => 'text-class',
-	                    'heading' => __( 'Text', 'text-domain' ),
-	                    'param_name' => 'text',
-	                    'value' => __( 'Default value', 'text-domain' ),
-	                    'description' => __( 'Box Text', 'text-domain' ),
-	                    'admin_label' => false,
-	                    'weight' => 0,
-	                    'group' => 'Custom Group',
-	                )                   
-	                     
-	            )
-	        )
-	    );                                
-	        
-	}     
-     
-	// Element HTML
-	public function vc_informationbox_html( $atts ) {
-	     
-	    // Params extraction
-	    extract(
-	        shortcode_atts(
-	            array(
-	                'title'   => '',
-	                'text' => '',
-	            ), 
-	            $atts
-	        )
-	    );
-	     
-	    // Fill $html var with data
-	    $html = '
-	    <div class="vc-informationbox-wrap">
-	     
-	        <h2 class="vc-informationbox-title">' . $title . '</h2>
-	         
-	        <div class="vc-informationbox-text">' . $text . '</div>
-	     
-	    </div>';      
-	     
-	    return $html;
-	     
-	}
-     
-} // End Element Class
- 
-// Element Class Init
-new vcInformationBox();  
+add_shortcode( 'bartag', 'bartag_func' );
+?>
+<?php
+add_action( 'vc_before_init', 'your_name_integrateWithVC' );
+function your_name_integrateWithVC() {
+	vc_map( array(
+		"name" => __( "Bar tag test", "my-text-domain" ),
+		"base" => "bartag",
+		"class" => "",
+		"category" => __( "Content", "my-text-domain"),
+		'admin_enqueue_js' => array(get_template_directory_uri().'/vc_extend/bartag.js'),
+		'admin_enqueue_css' => array(get_template_directory_uri().'/vc_extend/bartag.css'),
+		//'icon'     => get_template_directory_uri() . '/vendor/proteusthemes/visual-composer-elements/assets/images/pt.svg',
+				
+		"params" => array(
+			array(
+				"type" => "textfield",
+				"holder" => "div",
+				"class" => "",
+				"heading" => __( "Text", "my-text-domain" ),
+				"param_name" => "foo",
+				"value" => __( "Default param value", "my-text-domain" ),
+				"description" => __( "Description for foo param.", "my-text-domain" )
+				),
+			array(
+				"type" => "colorpicker",
+				"class" => "",
+				"heading" => __( "Text color", "my-text-domain" ),
+				"param_name" => "color",
+				"value" => '#FF0000', //Default Red color
+				"description" => __( "Choose text color", "my-text-domain" )
+			),
+			array(
+				"type" => "textarea_html",
+				"holder" => "div",
+				"class" => "",
+				"heading" => __( "Content", "my-text-domain" ),
+				"param_name" => "content", // Important: Only one textarea_html param per content element allowed and it should have "content" as a "param_name"
+				"value" => __( "<p>I am test text block. Click edit button to change this text.</p>", "my-text-domain" ),
+				"description" => __( "Enter your content.", "my-text-domain" )
+			)
+		)
+	));
+}
+?>
