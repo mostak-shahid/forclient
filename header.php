0e2114b76482eddba52874dac1c6770635d60eb6
Md. Mostak Shahid <mostak.shahid@gmail.com>
2019-12-16 12:58:45 +0600

    update

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="keywords" content="">
-	<meta name="description" content="">
+    <meta name="keywords" content="<?php echo get_post_meta( get_the_ID(),'_yoast_wpseo_focuskw', true ) ?>">
+    <meta name="description" content="<?php echo get_post_meta( get_the_ID(),'_yoast_wpseo_metadesc', true ) ?>">
 	<meta name="author" content="Md. Mostak Shahid">
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
