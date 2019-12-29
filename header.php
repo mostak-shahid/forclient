22251c6f065ec96f65ecf99321053a5cd6c85d19
Md. Mostak Shahid <mostak.shahid@gmail.com>
2019-12-29 12:48:02 +0600

    Update header.php

--- header.php
+++ header.php
@@ -34,7 +34,7 @@ else $page_id = get_the_ID();
 	$title_class=$forclient_options['sections-title-class']; 
 	$breadcrumbs_class=$forclient_options['sections-breadcrumbs-class']; 
 	?>
-	<header id="main-header" class="<?php echo $header_class?>">
+	<header id="main-header" class="<?php if(@$forclient_options['sections-header-background-type'] == 1) echo @$forclient_options['sections-header-background'] . ' ';?><?php if(@$forclient_options['sections-header-color-type'] == 1) echo @$forclient_options['sections-header-color'];?> <?php echo $header_class?>">
 		<div class="content-wrap">
 			<div class="container">
 				<nav class="navbar navbar-expand-md navbar-light navbar-custom-bg">			
