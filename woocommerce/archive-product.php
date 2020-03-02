d9b27e201532a0585bf43b3d9b562c9ad8e7d0af
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-03-02 12:19:24 +0600

    update

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -31,7 +31,7 @@ do_action( 'woocommerce_before_main_content' );
 ?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+		<!-- <h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1> -->
 	<?php endif; ?>
 
 	<?php
@@ -44,6 +44,21 @@ do_action( 'woocommerce_before_main_content' );
 	do_action( 'woocommerce_archive_description' );
 	?>
 </header>
+<section id="page" class="page-content">
+	<div class="content-wrap">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 order-fast">
+<?php 
+/**
+ * Hook: woocommerce_sidebar.
+ *
+ * @hooked woocommerce_get_sidebar - 10
+ */
+do_action( 'woocommerce_sidebar' );
+?>
+				</div>
+				<div class="col-lg-9 order-last">
 <?php
 if ( woocommerce_product_loop() ) {
 
@@ -87,19 +102,19 @@ if ( woocommerce_product_loop() ) {
 	 */
 	do_action( 'woocommerce_no_products_found' );
 }
-
+?>
+				</div>
+			</div>
+		</div>		
+	</div>
+</section>
+<?php 
 /**
  * Hook: woocommerce_after_main_content.
  *
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action( 'woocommerce_after_main_content' );
-
-/**
- * Hook: woocommerce_sidebar.
- *
- * @hooked woocommerce_get_sidebar - 10
- */
-do_action( 'woocommerce_sidebar' );
-
+?>
+<?php
 get_footer( 'shop' );
